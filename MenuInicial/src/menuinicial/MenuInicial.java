505df3caca6583a61b33a56317729d07ccbd6725
thiagomartins4-hash <thiago.martins4@estudante.ifms.edu.br>
2026-05-14 10:26:17 -0400

    menu inicial

--- MenuInicial/src/menuinicial/MenuInicial.java
+++ MenuInicial/src/menuinicial/MenuInicial.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package menuinicial;
 
 /**
@@ -14,7 +10,9 @@ public class MenuInicial {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Menu da Nalu!!!!!!!!");
+        System.out.println("1 - Cadastrar tarefa");
+        System.out.println("2 - Listar tarefas");
     }
     
 }
